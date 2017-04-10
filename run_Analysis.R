53b3f59300d7663f0f352a4d0e1d19227e2d090b
Kimtai Kiminy <sammykiminy@yahoo.com>
2017-04-10 15:02:17 +0300

    Update and rename Project. to run_Analysis.R

--- run_Analysis.R
+++ run_Analysis.R
@@ -12,7 +12,7 @@
 # 4. Appropriately label the data set with descriptive activity names. 
 # 5. From the set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject. 
 
-####################################################################
+##############################################
 
 
 # Clean up workspace
@@ -21,7 +21,7 @@ rm(list=ls())
 # 1. Merge the training and the test sets to create one data set.
 
 #set working directory to the location where the UCI HAR Dataset was unzipped
-setwd('/Users/heather/Documents/UCI HAR Dataset/');
+setwd('/Users/kim/Desktop/UCI HAR Dataset/');
 
 # Read in the data from files
 features     = read.table('./features.txt',header=FALSE); #imports features.txt
